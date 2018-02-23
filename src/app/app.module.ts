58a260f6943b969441a3e4ef0663b577ccd2801f
RaviDeveloper1 <aravi.us80@gmail.com>
2018-02-23 14:06:51 -0600

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+/* sub routing  */
+{ path: 'user', children: [
+  {path: 'list', component: UserListComponent, children: [
+    {path: 'detail/:name', component: UserComponent }
+  ]}
+] },
+
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
